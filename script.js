25739092ed9931e92e38eaa20a329db377de3b19
yassine-mami <96735910+yassine-mami@users.noreply.github.com>
2025-02-21 18:39:22 +0100

    script.js

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ hamburgerIcon.addEventListener('click', () => {
     // Toggle the 'menu-open' class on the body to disable scrolling
     document.body.classList.toggle('menu-open');
 });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzI1z82mjXhGQ35rKA7cc5V8Ohx0PK2pJK6X0SQzCqJISIQ__osP0G9JPKwCzHHors/exec'
+
+const form = document.forms['contactForm']
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(Response => alert("Thank you! Your message has been sended successfuly."))
+    .then(() => {window.location.reload(); })
+    .catch(error => console.error('Error', error.message))
+})
+
